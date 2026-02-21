8e566c910b44d0746b0428cba3acf31b780ebe09
Nicolas <nicolasboldt51@gmail.com>
2026-02-20 23:01:38 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log ("óla mundo")
+let nome = "nicolas"
+let idade = 17;
+console.log("seu nome é nicolas")
+console.log("sua idade é 17 anos")
+if (idade >= 18){
+    console.log("você é maior de idade");
+}else{
+    console.log ("você é menor de idade");
+}
+{let nota = 5
+if (nota>=7)
+    console.log("você está aprovado");
+else
+    console.log("você está reprovado");
+}
