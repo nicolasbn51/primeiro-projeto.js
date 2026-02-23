76863ab40232e20b45bd453e4ddcbfb85dc129ba
Nicolas <nicolasboldt51@gmail.com>
2026-02-23 14:01:43 -0300

    alteraco de codigo

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-console.log ("óla mundo")
+ ("óla mundo")
 let nome = "nicolas"
 let idade = 17;
-console.log("seu nome é nicolas")
-console.log("sua idade é 17 anos")
+("seu nome é nicolas")
+("sua idade é 17 anos")
 if (idade >= 18){
-    console.log("você é maior de idade");
+    ("você é maior de idade");
 }else{
-    console.log ("você é menor de idade");
+     ("você é menor de idade");
 }
 {let nota = 5
 if (nota>=7)
-    console.log("você está aprovado");
+    ("você está aprovado");
 else
-    console.log("você está reprovado");
-} bbb
+    ("você está reprovado");
+
+} 
